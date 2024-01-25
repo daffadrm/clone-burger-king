729191000d0055ab648e50770f62b8b3b617b505
daffadrm <daffarayhan18@gmail.com>
2024-01-25 14:20:02 +0700

    delete comment

--- components/widgets/Carousel.js
+++ components/widgets/Carousel.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Swipe from 'react-easy-swipe';
 import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,34 @@
-import Container from "@/components/layouts/Container";
-import DisplayCard from "@/components/widgets/DisplayCard";
-import CarouselDefault from "@/components/widgets/Carousel";
+import Container from '@/components/layouts/Container';
+import DisplayCard from '@/components/widgets/DisplayCard';
+import CarouselDefault from '@/components/widgets/Carousel';
 
-// import Image from "next/image";
-import { menusItem } from "@/contexts/menu-items";
+import { menusItem } from '@/contexts/menu-items';
 
 const slides = [
-  "https://bkdelivery.co.id/media/slider_image/2024/1/15/k5zx5lnjaqjykyqkmcpm2k.jpg",
-  "https://bkdelivery.co.id/media/slider_image/2023/11/15/wq6t8n4pyjrmgxkuko82gj.jpg",
-  "https://bkdelivery.co.id/media/slider_image/2023/10/25/sqpdvobbwsqgefrvfj3asb.jpg",
-  "https://bkdelivery.co.id/media/slider_image/2024/1/12/vym2tchnhmn7864ton9q54.jpg",
-  "https://bkdelivery.co.id/media/slider_image/2023/12/21/gahgzzdchy98rkwrrqbyt5.jpg",
-]
+	'https://bkdelivery.co.id/media/slider_image/2024/1/15/k5zx5lnjaqjykyqkmcpm2k.jpg',
+	'https://bkdelivery.co.id/media/slider_image/2023/11/15/wq6t8n4pyjrmgxkuko82gj.jpg',
+	'https://bkdelivery.co.id/media/slider_image/2023/10/25/sqpdvobbwsqgefrvfj3asb.jpg',
+	'https://bkdelivery.co.id/media/slider_image/2024/1/12/vym2tchnhmn7864ton9q54.jpg',
+	'https://bkdelivery.co.id/media/slider_image/2023/12/21/gahgzzdchy98rkwrrqbyt5.jpg',
+];
 export default function Home() {
-  return (
-    // <h2>tess</h2>
-    <Container title="Home">
-      {/* <Carousel autoSlide={true} >
-          {[...slides.map((s, index) => (
-            <img src={s} key={index}/>
-          ))]}
-        </Carousel> */}
-        <CarouselDefault images={slides}/>
-      <h2 className="font-primary text-branding-accent-secondary text-2xl text-center">
-        OUR MENUS
-      </h2>
+	return (
+		<Container title="Home">
+			<CarouselDefault images={slides} />
+			<h2 className="font-primary text-branding-accent-secondary text-2xl text-center">
+				OUR MENUS
+			</h2>
 
-      <section className="center-container lg:gap-4 lg:grid-cols-3 grid grid-cols-2 gap-3 px-4">
-        {menusItem.map(({ id, thumbnailUrl, displayName }) => (
-          <DisplayCard
-            key={id}
-            navigateTo={`/menus/${id}`}
-            title={displayName}
-            imageSrc={thumbnailUrl}
-          />
-        ))}
-      </section>
-    </Container>
-  );
+			<section className="center-container lg:gap-4 lg:grid-cols-3 grid grid-cols-2 gap-3 px-4">
+				{menusItem.map(({ id, thumbnailUrl, displayName }) => (
+					<DisplayCard
+						key={id}
+						navigateTo={`/menus/${id}`}
+						title={displayName}
+						imageSrc={thumbnailUrl}
+					/>
+				))}
+			</section>
+		</Container>
+	);
 }
